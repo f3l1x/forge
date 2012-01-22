2c3f184d06e2956d9da551b84c9f8c110398eff5
Milan Šulc <rkfelix@gmail.com>
2012-01-22 07:18:48 -0800

    Examples for Google Plusone

---
+++ nette/GooglePlusone/nette.example.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>GooglePlusone html test</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	{control googlePlusone:javascript}
+	</head>
+
+<body>
+{control googlePlusone}
+</body>
+
+</html>
