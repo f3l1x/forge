5efdacc8ad0b2761ef5ec40a8214707d4192a06c
Milan Sulc <felix@noblexity.com>
2012-06-05 12:02:38 +0200

    phpdocs update + readme added

--- GooglePlusone/GooglePlusone.php
+++ GooglePlusone/GooglePlusone.php
@@ -3,7 +3,7 @@
  * DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
  * Version 2, December 2004
  * 
- * Copyright (C) 2011 Milan Felix Sulc <rkfelix@gmail.com>
+ * Copyright (C) 2011 - 2012 Milan Felix Sulc <rkfelix@gmail.com>
  * 
  * Everyone is permitted to copy and distribute verbatim or modified
  * copies of this license document, and changing it is allowed as long
@@ -17,28 +17,69 @@
  */
 
 class GooglePlusone extends Nette\Object { 
-	
+
+    /**
+     * Size constants
+     */
+
 	const SIZE_SMALL = "small";
 	const SIZE_MEDIUM = "medium";
 	const SIZE_STANDART = "standart";
 	const SIZE_TALL = "tall";
-	
+
+    /**
+     * Annotation constants
+     */
 	const ANNOTATION_INLINE = "inline";
 	const ANNOTATION_BUBBLE = "bubble";
 	const ANNOTATION_NONE = "none";
-	
+
+    /**
+     * Google url
+     */
 	const GOOGLE_PLUSONE_URL = 'https://apis.google.com/js/plusone.js';
-	
+
+    /**
+     * @var string
+     */
 	public $size = self::SIZE_STANDART;
+
+    /**
+     * @var string
+     */
 	public $annotation = self::ANNOTATION_INLINE;
-	public $callback = null;
-	public $url;
-	public $html5 = FALSE;
-	public $lang = 'cs';
-	public $asynchronous = FALSE;
-	
-	public function render() {
-		
+
+    /**
+     * @var string|null
+     */
+    private $callback = null;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var bool
+     */
+    private $html5 = FALSE;
+
+    /**
+     * @var string
+     */
+    private $lang = 'cs';
+
+    /**
+     * @var bool
+     */
+	private $asynchronous = FALSE;
+
+    /**
+     * Renders Google +1 button
+     */
+    public function render() {
+
+        // Checks for html5 way..
 		if ($this->html5) {
 			$el = Nette\Utils\Html::el('div class=g-plusone');
 		} else {
@@ -59,11 +100,17 @@ class GooglePlusone extends Nette\Object {
 		echo $el;
 	}
 
-	public function renderJs() {
+    /**
+     * Alias for renderJavascript
+     */
+    public function renderJs() {
 		$this->renderJavascript();
 	}
-	
-	public function renderJavascript() {
+
+    /**
+     * Renders important google script
+     */
+    public function renderJavascript() {
 		
 		if ($this->asynchronous) {
 			$el = Nette\Utils\Html::el('script type="text/javascript"');
@@ -79,40 +126,132 @@ class GooglePlusone extends Nette\Object {
 			echo $el;
 		} 
 	}
-	
-	public function useHtml5() {
-		$this->html5 = TRUE;
-		return $this;
-	}
-	
-	public function setUrl($url) {
-		$this->url = $url;
-		return $this;
-	}
-	
-	public function setAnnotation($annotation) {
-		$this->annotation = $annotation;
-		return $this;
-	}
-	
-	public function setSize($size) {
-		$this->size = $size;
-		return $this;
-	}
-	
-	public function setCallback($callback) {
-		$this->callback = $callback;
-		return $this;
-	}
-	
-	public function setAsynchronous($asyn) {
-		$this->asynchronous = (boolean) $asyn;
-		return $this;
-	}
-	
-	public function setLang($lang) {
-		$this->lang = $lang;
-		return $this;
-	}
-	
+
+    /**
+     * @param $annotation
+     * @return GooglePlusone
+     */
+    public function setAnnotation($annotation)
+    {
+        $this->annotation = $annotation;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAnnotation()
+    {
+        return $this->annotation;
+    }
+
+    /**
+     * @param $asynchronous
+     * @return GooglePlusone
+     */
+    public function setAsynchronous($asynchronous)
+    {
+        $this->asynchronous = $asynchronous;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isAsynchronous()
+    {
+        return $this->asynchronous;
+    }
+
+    /**
+     * @param $callback
+     * @return GooglePlusone
+     */
+    public function setCallback($callback)
+    {
+        $this->callback = $callback;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCallback()
+    {
+        return $this->callback;
+    }
+
+    /**
+     * @param $html5
+     * @return GooglePlusone
+     */
+    public function setHtml5($html5)
+    {
+        $this->html5 = $html5;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isHtml5()
+    {
+        return $this->html5;
+    }
+
+    /**
+     * @param $lang
+     * @return GooglePlusone
+     */
+    public function setLang($lang)
+    {
+        $this->lang = $lang;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLang()
+    {
+        return $this->lang;
+    }
+
+    /**
+     * @param $size
+     * @return GooglePlusone
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param $url
+     * @return GooglePlusone
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+
 }
