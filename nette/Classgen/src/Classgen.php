bc0ac4f1a9f4add0adba1aed3e45561387034b9e
Milan Felix Sulc <rkfelix@gmail.com>
2014-12-09 14:40:02 +0100

    Classgen: cleanup

--- nette/Classgen/src/Classgen.php
+++ nette/Classgen/src/Classgen.php
@@ -1,158 +1,190 @@
 <?php
-class Classgen extends NControl
+
+use Nette\Application\UI\Control;
+use Nette\DI\Config\Adapters\NeonAdapter;
+use Nette\Utils\ArrayHash;
+use Nette\Utils\Arrays;
+use Nette\Utils\Strings;
+
+class Classgen extends Control
 {
-	public $template;
-	public $data;
-	
-	public function startup(){
-		$this->template = $this->getTemplate();
-		$this->template->setFile(dirname(__FILE__) . '/Classgen.latte');
-		$this->template->phpTag = "<?php";
-		$this->data = NConfigAdapterNeon::load(dirname(__FILE__).'/config.neon');
-	}
-		
-	public function generate(){
-		$this->startup();
+    public $template;
+    public $data;
 
-		$this->template->className = $this->data['className'];
-		
-		$this->convert();
+    public function startup()
+    {
+        $this->template = $this->getTemplate();
+        $this->template->setFile(dirname(__FILE__) . '/Classgen.latte');
+        $this->template->phpTag = "<?php";
 
-		$fileName = $this->data['fileName'];
-		$file = dirname(__FILE__).'/'.$fileName.'.php';
-		fopen($fileName, "w+");
-		file_put_contents($file, $this->template->__toString());
-	}
-	
-	public function convert(){
-		// konstanty
-		$this->processCons();
+        $neon = new NeonAdapter();
+        $this->data = $neon->load(dirname(__FILE__) . '/config.neon');
+    }
 
-		// promenne
-		$this->processVars();
+    public function generate()
+    {
+        $this->startup();
 
-		// settery
-		$this->processSetters();
+        $this->template->className = $this->data['className'];
 
-		// gettery
-		$this->processGetters();
+        $this->convert();
 
-		// funkce
-		$this->processFuncs();
+        $fileName = $this->data['fileName'];
+        $file = dirname(__FILE__) . '/' . $fileName . '.php';
+        fopen($fileName, "w+");
+        file_put_contents($file, $this->template->__toString());
+    }
 
-		// extends
-		$this->processExtends();
-		
-		//implements
-		$this->processImplements();
-	}
+    public function convert()
+    {
+        // konstanty
+        $this->processCons();
 
-	public function processVars(){
-		$arr = array();
-		foreach(NArrayTools::get($this->data,'variables',array()) as $name => $vars){
-			if(is_numeric($name)){ $name = $vars; $vars = NULL; }
-			$arr[] = $this->getVisibility($vars)."$$name".$this->getDefault($vars);
-		}
-		$this->template->variables = $arr;
-	}
+        // promenne
+        $this->processVars();
 
-	public function processCons(){
-		$arr = array();
-		foreach(NArrayTools::get($this->data,'constants',array()) as $name => $cons){
-			if($this->getDefault($cons) != ";"){
-				$arr[] = "$".NString::upper($name).$this->getDefault($cons);
-			}
-		}
-		$this->template->constants = $arr;
-	}	
+        // settery
+        $this->processSetters();
 
-	public function processSetters(){
-		$arr = array();
-		foreach(NArrayTools::get($this->data,'variables',array()) as $name => $vars){
-			$s1 = NArrayTools::get((array)$vars,'setter',true);
-			$s2 = NArrayTools::get((array)$vars,'auto',true);
-			if( ($s2 !== false) && ($s1 !== false)){
-				if(is_numeric($name)){ $name = $vars; $vars = NULL; }
-				$arr[] = $this->getVisibility($vars)."function set".NString::firstUpper($name)."($$name){\n\t\t\$this->$name = $$name;\n\t}\n";				
-			}
-		}
-		$this->template->setters = $arr;
-	}	
-		
-	public function processGetters(){
-		$arr = array();
-		foreach(NArrayTools::get($this->data,'variables',array()) as $name => $vars){
-			$g1 = NArrayTools::get((array)$vars,'getter',true);
-			$g2 = NArrayTools::get((array)$vars,'auto',true);
-			if( ($g2 !== false) && ($g1 !== false)){
-				if(is_numeric($name)){ $name = $vars; $vars = NULL; }
-				$arr[] = $this->getVisibility($vars)."function get".NString::firstUpper($name)."($$name){\n\t\treturn \$this->$name;\n\t}\n";				
-			}
-		}
-		$this->template->getters = $arr;
-	}	
+        // gettery
+        $this->processGetters();
 
-	public function processFuncs(){
-		$arr = array();
-		foreach(NArrayTools::get($this->data,'functions',array()) as $name => $fn){
-			$arr[] = $this->getVisibility($fn)."function $name(".$this->getParams($fn)."){\n\t}\n";
-		}
-		$this->template->functions = $arr;
-	}	
-	
-	public function processExtends(){
-		$this->template->extends = NArrayTools::get($this->data,'extends',false);
-	}
-	
-	public function processImplements(){
-		$imp = NArrayTools::get($this->data,'implements',false);
-		$imp = (is_array($imp) ? implode(', ',$imp) : $imp);
-		$this->template->implements = $imp;
-	}	
-	
-	public function getVisibility($vars){
-		if(!is_array($vars) || !NArrayTools::searchKey($vars,'visibility')){
-			return "public ";	
-		}else{
-			return NArrayTools::get($vars,'visibility')." ";
-		}
-	}
-	
-	public function getDefault($vars){
-		if($vars == NULL){
-			return ";";
-		}else if(!is_array($vars)){
-			return " = ".$this->parseValue($vars).";";
-		}else if(($default = NArrayTools::get($vars,'default',NULL)) != NULL){
-			return " = ".$this->parseValue($default).";";
-		}else{
-			return ";";
-		}
-	}
-	
-	public function parseValue($value){
-		if(is_numeric($value)){
-			return $value;
-		}else if($value == NULL){
-			return "NULL";
-		}else{
-			return "'".$value."'";
-		}		
-	}
-	
-	public function getParams($vars){
-		if($vars == NULL || count($vars)==0){
-			return "";
-		}else{
-			$arr = array();
-			foreach(NArrayHash::from($vars) as $key=>$item){
-				if(is_numeric($key)){
-					$arr[] = "$$item";	
-				}else{
-					$arr[] = "$$key = ".$this->parseValue($item);	
-				}
-			}
-			return implode(', ',$arr);
-		}
-	}
+        // funkce
+        $this->processFuncs();
+
+        // extends
+        $this->processExtends();
+
+        //implements
+        $this->processImplements();
+    }
+
+    public function processVars()
+    {
+        $arr = array();
+        foreach (Arrays::get($this->data, 'variables', array()) as $name => $vars) {
+            if (is_numeric($name)) {
+                $name = $vars;
+                $vars = NULL;
+            }
+            $arr[] = $this->getVisibility($vars) . "$$name" . $this->getDefault($vars);
+        }
+        $this->template->variables = $arr;
+    }
+
+    public function processCons()
+    {
+        $arr = array();
+        foreach (Arrays::get($this->data, 'constants', array()) as $name => $cons) {
+            if ($this->getDefault($cons) != ";") {
+                $arr[] = "$" . Strings::upper($name) . $this->getDefault($cons);
+            }
+        }
+        $this->template->constants = $arr;
+    }
+
+    public function processSetters()
+    {
+        $arr = array();
+        foreach (Arrays::get($this->data, 'variables', array()) as $name => $vars) {
+            $s1 = Arrays::get((array)$vars, 'setter', TRUE);
+            $s2 = Arrays::get((array)$vars, 'auto', TRUE);
+            if (($s2 !== FALSE) && ($s1 !== FALSE)) {
+                if (is_numeric($name)) {
+                    $name = $vars;
+                    $vars = NULL;
+                }
+                $arr[] = $this->getVisibility($vars) . "function set" . Strings::firstUpper($name) . "($$name){\n\t\t\$this->$name = $$name;\n\t}\n";
+            }
+        }
+        $this->template->setters = $arr;
+    }
+
+    public function processGetters()
+    {
+        $arr = array();
+        foreach (Arrays::get($this->data, 'variables', array()) as $name => $vars) {
+            $g1 = Arrays::get((array)$vars, 'getter', TRUE);
+            $g2 = Arrays::get((array)$vars, 'auto', TRUE);
+            if (($g2 !== FALSE) && ($g1 !== FALSE)) {
+                if (is_numeric($name)) {
+                    $name = $vars;
+                    $vars = NULL;
+                }
+                $arr[] = $this->getVisibility($vars) . "function get" . Strings::firstUpper($name) . "($$name){\n\t\treturn \$this->$name;\n\t}\n";
+            }
+        }
+        $this->template->getters = $arr;
+    }
+
+    public function processFuncs()
+    {
+        $arr = array();
+        foreach (Arrays::get($this->data, 'functions', array()) as $name => $fn) {
+            $arr[] = $this->getVisibility($fn) . "function $name(" . $this->getParams($fn) . "){\n\t}\n";
+        }
+        $this->template->functions = $arr;
+    }
+
+    public function processExtends()
+    {
+        $this->template->extends = Arrays::get($this->data, 'extends', FALSE);
+    }
+
+    public function processImplements()
+    {
+        $imp = Arrays::get($this->data, 'implements', FALSE);
+        $imp = (is_array($imp) ? implode(', ', $imp) : $imp);
+        $this->template->implements = $imp;
+    }
+
+    public function getVisibility($vars)
+    {
+        if (!is_array($vars) || !Arrays::searchKey($vars, 'visibility')) {
+            return "public ";
+        } else {
+            return Arrays::get($vars, 'visibility') . " ";
+        }
+    }
+
+    public function getDefault($vars)
+    {
+        if ($vars == NULL) {
+            return ";";
+        } else if (!is_array($vars)) {
+            return " = " . $this->parseValue($vars) . ";";
+        } else if (($default = Arrays::get($vars, 'default', NULL)) != NULL) {
+            return " = " . $this->parseValue($default) . ";";
+        } else {
+            return ";";
+        }
+    }
+
+    public function parseValue($value)
+    {
+        if (is_numeric($value)) {
+            return $value;
+        } else if ($value == NULL) {
+            return "NULL";
+        } else {
+            return "'" . $value . "'";
+        }
+    }
+
+    public function getParams($vars)
+    {
+        if ($vars == NULL || count($vars) == 0) {
+            return "";
+        } else {
+            $arr = array();
+            foreach (ArrayHash::from($vars) as $key => $item) {
+                if (is_numeric($key)) {
+                    $arr[] = "$$item";
+                } else {
+                    $arr[] = "$$key = " . $this->parseValue($item);
+                }
+            }
+            return implode(', ', $arr);
+        }
+    }
 }
